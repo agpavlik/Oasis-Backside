6943bfadd2ee0c09d509ca6e1c0c5ee5f45f4b93
agpavlik <agpavlik@gmail.com>
2023-11-17 23:44:11 -0500

    Add images

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -12,6 +12,7 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://qemdqbskhzstbqzdmpil.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
     </Row>
   );
 }
