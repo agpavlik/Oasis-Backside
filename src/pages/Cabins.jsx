8de862119c6d867703322e9ba3b7288a0444cd92
agpavlik <agpavlik@gmail.com>
2023-11-20 12:33:34 -0500

    Install React hook form and add form to CreateCabinForm

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,13 +3,17 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 //import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
+import { useState } from "react";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
   // useEffect(function () {
   //   getCabins().then((data) => console.log(data));
   // }, []);
-
   // Replace useEffect fetching data with ReactQuery in CabinTable
+
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <>
@@ -20,6 +24,10 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
