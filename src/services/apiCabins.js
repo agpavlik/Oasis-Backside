73e06960b6c7a259637fcdbb4928964d89e03ea5
agpavlik <agpavlik@gmail.com>
2023-11-19 20:40:53 -0500

    Implement cabin delete functionality with useMutation

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,11 +1,23 @@
 import supabase from "./supabase";
 
+// Function loads cabins from the data
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+// Function deletes cabin from the data
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
   }
 
   return data;
