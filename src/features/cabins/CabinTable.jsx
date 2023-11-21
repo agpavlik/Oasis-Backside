c7195bfc132c2f2a17303fd73a61a205864d48a4
agpavlik <agpavlik@gmail.com>
2023-11-21 00:04:43 -0500

    Add custom hook useCabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
-import { getCabins } from "../../services/apiCabins";
-import { useQuery } from "@tanstack/react-query";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
+import { useCabins } from "./useCabins";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -30,14 +29,7 @@ const TableHeader = styled.header`
 
 function CabinTable() {
   //  destructure three important pieces of data.
-  const {
-    isLoading,
-    data: cabins,
-    error,
-  } = useQuery({
-    queryKey: ["cabins"],
-    queryFn: getCabins,
-  });
+  const { isLoading, cabins } = useCabins();
 
   if (isLoading) return <Spinner />;
 
