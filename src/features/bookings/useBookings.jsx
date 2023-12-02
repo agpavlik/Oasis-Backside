47177cac00ec053c207f6f5b6f9b8d2c6897077b
agpavlik <agpavlik@gmail.com>
2023-12-01 22:42:13 -0500

    Add API-side filtering

--- src/features/bookings/useBookings.jsx
+++ src/features/bookings/useBookings.jsx
@@ -1,14 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  // const queryClient = useQueryClient();
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
+  // // SORT
+  // const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  // const [field, direction] = sortByRaw.split("-");
+  // const sortBy = { field, direction };
+
+  // QUERY
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, error, bookings };
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,4 +1,5 @@
 import BookingTable from "../features/bookings/BookingTable";
+import BookingTableOperations from "../features/bookings/BookingTableOperations";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
@@ -7,6 +8,7 @@ function Bookings() {
     <>
       <Row type="horizontal">
         <Heading as="h1">All bookings</Heading>
+        <BookingTableOperations />
       </Row>
       <BookingTable />
     </>
